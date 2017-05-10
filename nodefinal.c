149b87c3467f1d20d49c8114ce03c7d1c94d6c47
Diaztanto <diaztanto.h@gmail.com>
2017-05-10 15:03:22 +0700

    Udah ada kirim server

--- nodefinal.c
+++ nodefinal.c
@@ -28,7 +28,7 @@
   3/x/y is server update, x is server id, y is load
   4/x is prime number request, x is the request for the server
 
-  9/x 9 is vote req, x is the id number of the request sender
+  9/x/y 9 is vote req, x is the id number of the request sender, y is term number
 */
 
 int id;
@@ -37,6 +37,9 @@ int timer;
 int status; // 0 keeps running, 1 kills self
 clock_t start, end;
 
+CURL *curl;
+CURLcode res; 
+
 typedef struct {
   int id;
   char* address;
@@ -44,7 +47,7 @@ typedef struct {
 } server_load;
 
 typedef struct {
-  server_load* server;
+  server* servers;
 } server_list;
 
 int answer_to_connection (void *cls, struct MHD_Connection *connection,
@@ -55,24 +58,63 @@ int answer_to_connection (void *cls, struct MHD_Connection *connection,
 {
 //reset timer
   start = end;
+  char* page;
+  char* URL;
+  int intLength;
+
 
 //proses request
   if (strcmp(url, END) >= 0) {
     status = 1;
-    const char *page  = "Farewell.";
+    page = (char*) malloc (sizeof(char) * 10);
+    page = "Farewell.";
   } else if (url[0] == '9' && url[1] == '/') {
     //parse the 8 and / out of the request and add the x for the response
     //tambah kondisi kalo dia udah vote ato belum
-    const char *page = (char*) malloc (sizeof(char) * (sizeof(url)-2));
+    page = (char*) malloc (sizeof(char) * (sizeof(url)-2));
 
     int i = 0;
     for (i = 0; i < sizeof(page); i++) {
-      page[0] = url[2];
+      page[i] = url[i+2];
     }
-  } if (url[0] == '3' && url[1] == '/') {
+  } else if (url[0] == '3' && url[1] == '/') {
     //update server load
-    const char *page  = "<insert appropriate response here>";
-  } else 
+    page = (char*) malloc (sizeof(char) * 9);
+    page  = "Success.";
+  } else if (url[0] == '4' && url[1] == '/') {
+    //prime number req
+    char* intS = (char*) malloc (sizeof(char) * (sizeof(url)-2));
+
+    int i = 0;
+    for (i = 0; i < sizeof(intS); i++) {
+      intS[i] = url[i+2];
+    }
+
+    NthPrime = atoi(intS);
+    intLength = (floor(log10(abs(NthPrime)))) + 1;
+
+    URL = (char*)malloc(sizeof(char) * (strlen("http://localhost:13337/") + intLength + 1));
+
+    curl = curl_easy_init();
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, URL);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, processResponse);  
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, ActualPrime);
+
+        res = curl_easy_perform(curl);
+        if(res != CURLE_OK) {
+          fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        }
+     
+      curl_easy_cleanup(curl);
+    }
+
+
+    page = (char*) malloc (sizeof(char) * 9);
+    page  = "Success.";
+  } else {
+    //coba kira2 respon apa lagi
+  }
 
 //need appropriate response tergantung itu request apa dan dari siapa (?)
   struct MHD_Response *response;
@@ -95,7 +137,7 @@ int main (int argc, char* argv[]){
   struct MHD_Daemon *daemon;
   int timeout;
   int randr;
-
+  
   status = 0;
 
 //get assigned id
@@ -108,11 +150,11 @@ int main (int argc, char* argv[]){
 
 //init serverlist
   server_list list;
-  list.server = (server*) malloc (sizeof(server) * SERVNUM);
+  list.servers = (server_load*) malloc (sizeof(server_load) * SERVNUM);
 
   int i;
   for (i = 0; i < SERVNUM; i++) {
-    list.server[i]->address = (char*) malloc (sizeof(char) * ADDRSIZE);
+    list.servers.address = (char*) malloc (sizeof(char) * ADDRSIZE);
   }
 
 //init role
@@ -146,7 +188,7 @@ int main (int argc, char* argv[]){
       printf("%d\n", timer);
     }
 
-//election!
+//become a candidate
     //insert code here about sending http request ke node lain using curl (yeah masih pake curl)
 
 
@@ -157,16 +199,19 @@ int main (int argc, char* argv[]){
 
 
     //insert code about starting heartbeat
+
+//become a leader
+    //do what a leader should do
   }
 
 
   getchar (); //<- ini bisa dibuang kalo dah ga perlu
 
   for (i = 0; i < SERVNUM; i++) {
-    free(list.server[i]->address)
+    free(list.servers[i].address);
   }
 
-  free(list.server);
+  free(list.servers);
 
   MHD_stop_daemon (daemon);
   return 0;
